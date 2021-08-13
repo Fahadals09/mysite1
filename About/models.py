ff47a077f1b75c6616bfed432700d49c7f352b47
fahadals09 <fahadals1100@gmail.com>
2021-08-13 20:05:56 +0300

    analysis

--- About/models.py
+++ About/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class About(models.Model) :
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='posts/')
+    description = models.TextField(max_length=3000)
+
+    def __str__(self):
+        return  self.title 
+
