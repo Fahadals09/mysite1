f1949468d4b3e1cdc7ecdb32e075bb4cd3835726
fahadals09 <fahadals1100@gmail.com>
2021-08-30 16:15:55 +0300

    try to add pic

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import contact
 # Create your views here.
 
 
 def Contact(request) :  
-    pass 
+    name = request.POST['name'] 
+    email = request.POST['email']
+    message = request.POST['message']
+    contact.objects.create(
+        Name = name,
+        Email = email,
+        Message = message,
+        
+    )
+    return redirect('/Home')
+
+
+    
+
